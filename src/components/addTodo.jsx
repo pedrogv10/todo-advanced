ea3c16b28e1a3af736be29496d868c1bcabd7b34
Pedro Henrique <pedrogv133@gmail.com>
2024-02-22 20:21:25 -0300

    feat: Update styles for add todo button

--- src/components/addTodo.jsx
+++ src/components/addTodo.jsx
@@ -48,18 +48,9 @@ const AddTask = ({ addTodo }) => {
           type="button"
           id="addDescription"
           onClick={() => setDescriptionInput(!descriptionInput)}>
-          <svg
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <circle cx="10" cy="10" r="10" fill="#1E1E1E" />
-            <path
-              d="M9.5625 14.2614V6.57955H11.0909V14.2614H9.5625ZM6.48864 11.1818V9.65341H14.1705V11.1818H6.48864Z"
-              fill="#42E8D8"
-            />
-          </svg>
+          <div className="plus-round">
+            <p>{!descriptionInput ? "+" : "-"}</p>
+          </div>
           <p>Add description</p>
         </button>
 
